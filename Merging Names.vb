2be3a7cfac269a1328c35640115aec175ce405c0
Zeeshan Ahmed <55400807+Zeeshanahmed2002@users.noreply.github.com>
2019-10-02 09:36:03 -0700

    Add files via upload

---
+++ Merging Names.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim S1, S2, Firstword, Secword As String
+        Dim SP As Integer
+
+        S1 = ""
+        S2 = ""
+        Firstword = ""
+        Secword = ""
+        SP = 0
+
+        Console.Write(" Enter your First string = ")
+        S1 = Console.ReadLine
+        Console.Write("Enter the Second string = ")
+        S2 = Console.ReadLine
+
+
+        SP = InStr(S1, " ")
+        Firstword = Left(S1, SP - 1)
+        SP = InStr(S2, " ")
+        Secword = Right(S2, Len(S2) - SP)
+
+
+        Console.WriteLine("Final String = " _
+                          & Firstword & " " & Secword)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
