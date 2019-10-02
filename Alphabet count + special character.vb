82cc919d7feda52f30d559f91293eac879b1cd68
Zeeshan Ahmed <55400807+Zeeshanahmed2002@users.noreply.github.com>
2019-10-02 09:55:04 -0700

    Add files via upload

---
+++ Alphabet count + special character.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Nextchar As String
+        Dim AlphabetCounter, NumberCounter, Scharacter, Counter As Integer
+
+
+        Str1 = ""
+        Nextchar = ""
+        AlphabetCounter = 0
+        NumberCounter = 0
+        Counter = 0
+        Scharacter = 0
+
+        Console.Write(" Enter string = ")
+        Str1 = Console.ReadLine
+
+        For Count = 1 To Len(Str1)
+            Nextchar = UCase(Mid(Str1, Count, 1))
+
+            If Nextchar >= "A" And Nextchar <= "Z" Then
+                AlphabetCounter = AlphabetCounter + 1
+            Else
+
+                If Nextchar >= "0" And Nextchar <= "9" Then
+                    NumberCounter = NumberCounter + 1
+                Else
+                    Scharacter = Scharacter + 1
+
+                End If
+
+
+            End If
+
+        Next
+
+
+        Console.WriteLine("The number of Alphabets in the string are = " & AlphabetCounter)
+        Console.WriteLine("The number of character other than Alphabet are = " & NumberCounter)
+        Console.WriteLine("The number of Special characters are = " & Scharacter)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
