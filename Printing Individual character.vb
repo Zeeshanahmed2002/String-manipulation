c56cbb759989a5ea3c922cec7bfc05a4409d82c0
Zeeshan Ahmed <55400807+Zeeshanahmed2002@users.noreply.github.com>
2019-10-03 06:22:23 -0700

    Add files via upload

---
+++ Printing Individual character.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String = ""
+        Dim Nextchar As Char = ""
+        Dim Counter As Integer = 0
+
+        Console.Write("Enter string = ")
+        Str1 = Console.ReadLine
+
+        For Counter = 1 To Len(Str1)
+            Nextchar = Mid(Str1, Counter, 1)
+            Console.WriteLine(Nextchar)
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
