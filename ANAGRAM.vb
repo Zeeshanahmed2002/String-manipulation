d4725101c56b03bf0f4c8f2e41ff827204247c73
Zeeshan Ahmed <55400807+Zeeshanahmed2002@users.noreply.github.com>
2019-10-12 01:16:29 -0700

    Add files via upload

---
+++ ANAGRAM.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim firstword As String = ""
+        Dim secondword As String = ""
+        Dim count As Integer = 0
+        Dim Newchar As Char = ""
+        Dim flag As Boolean = True
+
+        Console.Write("Enter Firstword = ")
+        firstword = Console.ReadLine
+        firstword = UCase(firstword)
+
+
+        Console.Write("Enter secondword = ")
+        secondword = Console.ReadLine
+        secondword = UCase(secondword)
+
+        For count = 1 To Len(firstword)
+            Newchar = Mid(firstword, count, 1)
+            If InStr(secondword, Newchar) = 0 Then
+                flag = False
+                Exit For
+            End If
+        Next
+
+        If flag = True Then
+            Console.Write("These words are Anagram!")
+        Else
+            Console.Write("These words are NOT Anagram!")
+        End If
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
---
+++ INVERSING STRING.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String = ""
+        Dim Str2 As String = ""
+        Console.Write("Enter string to reverse = ")
+        Str1 = Console.ReadLine
+
+
+
+        If Str1 = "" Then
+            Console.Write("Try a valid name please.")
+        Else
+            Dim Name As String = Str1
+            Dim s As String = ""
+            For i As Integer = Name.Length To 1 Step -1
+                s = s & Mid(Name, i, 1)
+            Next
+            Str2 = s
+        End If
+
+        Console.Write(Str2)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
