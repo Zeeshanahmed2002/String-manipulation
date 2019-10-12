afb8c81ca6013903b4a6cd5181e23b9b5fd581d8
Zeeshan Ahmed <55400807+Zeeshanahmed2002@users.noreply.github.com>
2019-10-12 01:19:16 -0700

    Add files via upload

---
+++ ISOLATED DATA TYPES.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Nextchar, Alphabet, Number, Scharacter As String
+        Dim Counter As Integer = 0
+
+
+        Str1 = ""
+        Nextchar = ""
+        Alphabet = ""
+        Number = ""
+        Scharacter = ""
+
+        Console.Write(" Enter string = ")
+        Str1 = Console.ReadLine
+
+        For Count = 1 To Len(Str1)
+            Nextchar = UCase(Mid(Str1, Count, 1))
+
+            If Nextchar >= "A" And Nextchar <= "Z" Then
+                Alphabet = Alphabet & Nextchar
+            Else
+
+                If Nextchar >= "0" And Nextchar <= "9" Then
+                    Number = Number & Nextchar
+                Else
+                    Scharacter = Scharacter & Nextchar
+
+                End If
+
+
+            End If
+
+        Next
+
+
+        Console.WriteLine("The Alphabets in the string are = " & Alphabet)
+        Console.WriteLine("The character other than Alphabet are = " & Number)
+        Console.WriteLine("The Special characters are = " & Scharacter)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
