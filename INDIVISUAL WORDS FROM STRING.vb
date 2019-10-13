ba25607ffc1b4fc94443babb63708da4265ed679
Zeeshan Ahmed <55400807+Zeeshanahmed2002@users.noreply.github.com>
2019-10-13 02:15:26 -0700

    This program prints individual words from the string

---
+++ INDIVISUAL WORDS FROM STRING.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String = ""
+        Dim Str2 As String = ""
+        Dim Nextchar As String = ""
+        Dim count As Integer = 0
+
+        Console.Write("Enter string = ")
+        Str1 = Console.ReadLine
+
+        For count = 1 To Len(Str1)
+            Nextchar = Mid(Str1, count, 1)
+            If Nextchar = " " Then
+                Console.WriteLine(Str2)
+                Str2 = ""
+            Else
+                Str2 = Str2 & Nextchar
+            End If
+        Next
+        Console.WriteLine(Str2)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
